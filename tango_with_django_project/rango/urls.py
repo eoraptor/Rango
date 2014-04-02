06e88c42e0a8f754531914ca527045e53b3f3d17
Fiona Steven <f.steven@ntlworld.com>
2014-04-02 20:30:43 +0100

    Chapter 6

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
 from rango import views
 
+
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url('^about/', views.about, name='about'))
+                       url('^about/$', views.about, name='about'),
+                       url('^category/(?P<category_name_url>\w+)/$', views.category, name="category"),
+                       url('^add_category/$', views.add_category, name='add_category'),
+                       url('^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,26 @@
 from django.http import HttpResponse
 from django.template  import RequestContext
-from django.shortcuts import  render_to_response
+from django.shortcuts import render_to_response
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+
+def encode(category_name):
+    return category_name.name.replace(' ', '_')
+
+def decode(category_url):
+    return category_url.replace('_', ' ')
+
 
 def index(request):
     context = RequestContext(request)
 
-    context_dict = {'boldmessage': "I am a bold font from the context"}
+    category_list = Category.objects.order_by('-likes') [:5]
+    page_list = Page.objects.order_by('-views') [:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    for category in category_list:
+        category.url = encode(category)
+        #category.url = category.name.replace(' ', '_')
 
     return render_to_response('rango/index.html', context_dict, context)
 
@@ -17,3 +32,77 @@ def about(request):
     context_dict = {'boldmessage': "The about page is cool!"}
 
     return render_to_response('rango/about.html', context_dict, context)
+
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = category_name_url.replace('_', ' ')
+
+    context_dict = {'category_name' : category_name, 'category_name_url' : category_name_url}
+
+    try:
+
+        category = Category.objects.get(name=category_name)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category_name
+
+    except Category.DoesNotExist:
+
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
+
+
+def add_category(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            return index(request)
+
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+
+    return render_to_response('rango/add_category.html', {'form':form}, context)
+
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = decode(category_name_url)
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+
+            page = form.save(commit=False)
+
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.category = cat
+            except Category.DoesNotExist:
+                return  render_to_response('rango/add_category.html', {}, context)
+
+            page.views = 0
+
+            page.save()
+
+            return category(request, category_name_url)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html', {'category_name_url': category_name_url,
+        'category_name' : category_name, 'form' : form}, context)
